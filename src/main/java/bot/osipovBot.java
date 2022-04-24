2b1c14fbfaf16a4e5789feca81a960ae01a311f2
osipovmr <osipovmr>
2022-04-24 21:42:29 +0300

    add /help

--- src/main/java/bot/osipovBot.java
+++ src/main/java/bot/osipovBot.java
@@ -11,24 +11,25 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 public class osipovBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
+        //Извлекаем из объекта сообщение пользователя
+        Message inMess = update.getMessage();
+        //Достаем из inMess id чата пользователя
+        String chatId = inMess.getChatId().toString();
+        String userName = getUserName(inMess);
+        //Получаем текст сообщения пользователя, отправляем в написанный нами обработчик
+        String response = parseMessage(inMess.getText());
+        //Создаем объект класса SendMessage - наш будущий ответ пользователю
+        SendMessage outMess = new SendMessage();
+
         try {  // We check if the update has a message and the message has text
-            if (update.hasMessage() && update.getMessage().hasText()) {
-                //Извлекаем из объекта сообщение пользователя
-                Message inMess = update.getMessage();
-                //Достаем из inMess id чата пользователя
-                String chatId = inMess.getChatId().toString();
-                String userName = getUserName(inMess);
-                //Получаем текст сообщения пользователя, отправляем в написанный нами обработчик
-                String response = parseMessage(inMess.getText());
-                //Создаем объект класса SendMessage - наш будущий ответ пользователю
-                SendMessage outMess = new SendMessage();
+            if (update.hasMessage() && inMess.hasText()) {
                 //Добавляем в наше сообщение id чата а также наш ответ
                 outMess.setChatId(chatId);
-                outMess.setText(response + userName+ ".");
+                outMess.setText(response + userName + ".");
                 //Отправка в чат
                 execute(outMess);
             }
-        }// Call method to send the message
+        }
         catch (TelegramApiException e) {
             e.printStackTrace();
         }
@@ -44,8 +45,10 @@ public class osipovBot extends TelegramLongPollingBot {
         String response;
         if (textMsg.equals("/start"))
             response = "Приветствую, ";
+        else if (textMsg.equals("/help"))
+            response = "Чтобы получить помощь, обратитесь к пользователю osipov_mr, ";
         else
-            response = "Сообщение не распознано, нажмите /start, гражданин ";
+            response = "Сообщение не распознано, нажмите /start или /help, гражданин ";
 
         return response;
     }
